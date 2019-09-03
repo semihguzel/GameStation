3359923949939b25a0716a0a2e18bf2faf69d561
Semih Guzel <semih_guzel@outlook.com>
2019-09-03 12:09:43 +0300

    OrderDetail mappings have been configured.

--- GameStation.DAL/Mappings/OrderDetailMapping.cs
+++ GameStation.DAL/Mappings/OrderDetailMapping.cs
@@ -11,7 +11,13 @@ namespace GameStation.DAL.Mappings
     {
         public void Configure(EntityTypeBuilder<OrderDetail> builder)
         {
-            throw new NotImplementedException();
+            builder.ToTable("OrderDetails");
+            builder.HasKey(x => x.OrderDetailID);
+            builder.Property(x => x.UnitPrice).HasColumnType("money");
+            builder.Property(x => x.TotalAmount).HasColumnType("money");
+            builder.Property(x => x.OrderDate).HasColumnType("datetime2");
+
+            builder.HasMany(x => x.Orders).WithOne(x => x.OrderDetail).HasForeignKey(x => x.OrderDetailID);
         }
     }
 }
