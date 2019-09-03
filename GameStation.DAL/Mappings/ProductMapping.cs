389906dfe3f017de8e37a3c48aa0e39ca9469522
Semih Guzel <semih_guzel@outlook.com>
2019-09-03 12:26:04 +0300

    Product mappings have been configured.

--- GameStation.DAL/Mappings/ProductMapping.cs
+++ GameStation.DAL/Mappings/ProductMapping.cs
@@ -11,7 +11,28 @@ namespace GameStation.DAL.Mappings
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-            throw new NotImplementedException();
+            builder.ToTable("Products");
+            builder.HasKey(x => x.ProductID);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.QuantityPerUnit).HasMaxLength(50);
+            builder.Property(x => x.UnitPrice).IsRequired().HasColumnType("money");
+            builder.Property(x => x.OldPrice).HasColumnType("money");
+            builder.Property(x => x.UnitWeight).HasMaxLength(30);
+            builder.Property(x => x.Size).HasMaxLength(30);
+            builder.Property(x => x.ImageUrl).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.AltText).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.ShortDescription).IsRequired().HasMaxLength(300);
+            builder.Property(x => x.LongDescription).IsRequired().HasMaxLength(1500);
+            builder.Property(x => x.Picture1).HasMaxLength(500);
+            builder.Property(x => x.Picture2).HasMaxLength(500);
+            builder.Property(x => x.Picture3).HasMaxLength(500);
+            builder.Property(x => x.Picture4).HasMaxLength(500);
+            builder.Property(x => x.Notes).HasMaxLength(250);
+
+            builder.HasMany(x => x.OrderDetails).WithOne(x => x.Product).HasForeignKey(x => x.ProductID);
+            builder.HasMany(x => x.RecentlyViews).WithOne(x => x.Product).HasForeignKey(x => x.ProductID);
+            builder.HasMany(x => x.Reviews).WithOne(x => x.Product).HasForeignKey(x => x.ProductID);
+            builder.HasMany(x => x.WishLists).WithOne(x => x.Product).HasForeignKey(x => x.ProductID);
         }
     }
 }
