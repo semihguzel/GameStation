23dbc75f383796150e2a0334b137993176662202
Semih Guzel <semih_guzel@outlook.com>
2019-09-10 12:28:12 +0300

    Navbar added via ViewComponent.

---
+++ GameStation.WebUI/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameStation.WebUI.ViewComponents
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
